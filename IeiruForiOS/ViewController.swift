b16a2d2e1fc44bdfd2fb7c83e65a22f124f43b09
kantapro2020 <kantapro2020@gmail.com>
2020-06-15 23:05:44 +0900

    Initial Commit

---
+++ IeiruForiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IeiruForiOS
+//
+//  Created by 蛭間寛大 on 2020/06/15.
+//  Copyright © 2020 蛭間寛大. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ IeiruForiOSTests/IeiruForiOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  IeiruForiOSTests.swift
+//  IeiruForiOSTests
+//
+//  Created by 蛭間寛大 on 2020/06/15.
+//  Copyright © 2020 蛭間寛大. All rights reserved.
+//
+
+import XCTest
+@testable import IeiruForiOS
+
+class IeiruForiOSTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
