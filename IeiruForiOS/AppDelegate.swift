92ebf786ac39d4e6c463997a004b77e9050bbab4
kantapro2020 <kantapro2020@gmail.com>
2020-06-30 15:35:52 +0900

    view_update

--- IeiruForiOS/AppDelegate.swift
+++ IeiruForiOS/AppDelegate.swift
@@ -2,24 +2,35 @@
 //  AppDelegate.swift
 //  IeiruForiOS
 //
-//  Created by 蛭間寛大 on 2020/06/15.
-//  Copyright © 2020 蛭間寛大. All rights reserved.
+//  Created by KenzaburoTakagi on 2020/05/04.
+//  Copyright © 2020 KenzaburoTakagi. All rights reserved.
 //
-
 import UIKit
+import CoreLocation
+import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate, UNUserNotificationCenterDelegate {
 
-
+    let locationManager = CLLocationManager()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // 通知許可の取得
+        UNUserNotificationCenter.current().requestAuthorization(
+        options: [.alert, .sound, .badge]){
+            (granted, _) in
+            if granted{
+                UNUserNotificationCenter.current().delegate = self
+            }
+        }
+        
+        locationManager.startMonitoringSignificantLocationChanges()
         return true
     }
 
     // MARK: UISceneSession Lifecycle
-
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
@@ -31,7 +42,32 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        
+        if CLLocationManager.significantLocationChangeMonitoringAvailable() {
+            locationManager.startMonitoringSignificantLocationChanges()
+        }
+    }
+    
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        locationManager.startMonitoringSignificantLocationChanges()
+        let location = locationManager.location
 
-
+        print("**************************")
+        print(location?.coordinate.latitude)
+        print(location?.coordinate.longitude)
+        return true
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let content = UNMutableNotificationContent()
+        content.title = "お知らせ"
+        content.body = "位置情報アップデート中"
+        content.sound = UNNotificationSound.default
+        
+        // 直ぐに通知を表示
+        let request = UNNotificationRequest(identifier: "immediately", content: content, trigger: nil)
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+    }
 }
-
--- IeiruForiOS/ViewController.swift
+++ IeiruForiOS/ViewController.swift
@@ -6,157 +6,164 @@
 //  Copyright © 2020 蛭間寛大. All rights reserved.
 import UIKit
 import CoreLocation
-//httpリクエスト
-import Foundation
 
 /// Location の View
 class ViewController: UIViewController {
-    
-    
+    //　nameテキストフィールド
     @IBOutlet weak var name: UITextField!
-    /// 緯度を表示するラベル
-    @IBOutlet weak var latitude: UILabel!
-    /// 経度を表示するラベル
-    @IBOutlet weak var longitude: UILabel!
-    @IBOutlet weak var NameList: UILabel!
+    // startbtn
+    @IBOutlet weak var startBtn: UIButton!
+    // finishbtn
+    @IBOutlet weak var finishBtn: UIButton!
+    //　user一覧表示
+    @IBOutlet weak var userLabel: UILabel!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setupLocationManager()
-    }
-    @IBAction func SendBtn(_ sender: Any) {
-        let inputText = name.text
-        NameList.text = inputText
-        func searchGithubUser(query: String) {
-
-            let url = URL(string: "https://api.github.com/search/users?q=" + query)!
-            let request = URLRequest(url: url)
-        }
-    }
-    
-    // 緯度
-    var latitudeNow: String = ""
-    // 経度
-    var longitudeNow: String = ""
-    
-    /// ロケーションマネージャ
     var locationManager: CLLocationManager!
+    var latitude: Double?
+    var longitude: Double?
+    var usersIeiru: String?
     
+    let urlString = "http://18.176.193.22/users"
     
+    override func viewDidload() {
+        super.viewDidLoad()
     
-    /// "位置情報を取得"ボタンを押下した際、位置情報をラベルに反映する
-    /// "位置情報を取得"ボタンを押下した際、位置情報をラベルに反映する
-    /// - Parameter sender: "位置情報を取得"ボタン
-    @IBAction func TupSendBtn(_ sender: Any) {
-        // マネージャの設定
-        let status = CLLocationManager.authorizationStatus()
-        if status == .denied {
-            showAlert()
-        } else if status == .authorizedAlways {
-            self.latitude.text = latitudeNow
-            self.longitude.text = longitudeNow
-        }
-
-                let Url = String(format: "http://18.176.193.22/users/")
-                    guard let serviceUrl = URL(string: Url) else { return }
-                    let parameters: [String: Any] = [
-                        "user": [
-                            "name" : NameList.text,
-                            "longitude" : latitudeNow,
-                            "latitude": longitudeNow
-                        ]
-                    ]
-                    var request = URLRequest(url: serviceUrl)
-                    request.httpMethod = "POST"
-                    request.setValue("Application/json", forHTTPHeaderField: "Content-Type")
-                    guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else {
-                        return
-                    }
-                    request.httpBody = httpBody
-                    request.timeoutInterval = 20
-                    let session = URLSession.shared
-                    session.dataTask(with: request) { (data, response, error) in
-                        if let response = response {
-                            print(response)
-                        }
-                        if let data = data {
-                            do {
-                                let json = try JSONSerialization.jsonObject(with: data, options: [])
-                                print(json)
-                            } catch {
-                                print(error)
-                            }
-                        }
-                    }.resume()
-        
-        
-    }
-    //リセットボタン
-    @IBAction func Clear(_ sender: Any) {
-        self.latitude.text = "デフォルト"
-        self.longitude.text = "デフォルト"
+        getLocationInfo(completion: { lines in
+            DispatchQueue.main.async {
+                // labelの出し方は要検討
+                let LineHeightStyle = NSMutableParagraphStyle()
+                let lineSpaceSize = CGFloat(10)
+                LineHeightStyle.lineSpacing = lineSpaceSize
+                let lineHeightAttr = [NSAttributedString.Key.paragraphStyle: LineHeightStyle]
+                self.usersLabel.frame.size.height = (UIFont.systemFontSize + lineSpaceSize) * CGFloat(lines + 3)
+                self.usersLabel.attributedText = NSMutableAttributedString(string: self.usersIeiru ?? "", attributes: lineHeightAttr)
+            }
+        })
     }
     
+    override func viewDidLayoutSubviews() {
+        usersLabel.text = usersIeiru
+    }
     
-    /// ロケーションマネージャのセットアップ
+    @IBAction func tapStartBtn(_ sender: Any) {
+        setupLocationManager()
+        postLocationInfo()
+    }
+    
+    @IBAction func tapFinishBtn(_ sender: Any) {
+        locationManager.stopUpdatingLocation()
+    }
+    
     func setupLocationManager() {
         locationManager = CLLocationManager()
-        
-        // 権限をリクエスト
         guard let locationManager = locationManager else { return }
-//        アプリを使用中のみ許可
-//        locationManager.requestWhenInUseAuthorization()
-//        常に許可
         locationManager.requestAlwaysAuthorization()
         
-        // マネージャの設定
         let status = CLLocationManager.authorizationStatus()
-        
-        // ステータスごとの処理
         if status == .authorizedAlways {
             locationManager.delegate = self
+            locationManager.requestLocation()
             locationManager.startUpdatingLocation()
+            locationManager.allowsBackgroundLocationUpdates = true
+        } else {
+            showAlert()
         }
     }
-    
-    /// アラートを表示する
+
     func showAlert() {
-        let alertTitle = "位置情報取得が許可されていません。"
-        let alertMessage = "設定アプリの「プライバシー > 位置情報サービス」から変更してください。"
+        let alertTitle = "位置情報取得が許可されてないンゴ"
+        let alertMessage = "設定アプリの「プライバシー > 位置情報サービス」から常に許可にしてね"
         let alert: UIAlertController = UIAlertController(
-            title: alertTitle,
-            message: alertMessage,
-            preferredStyle:  UIAlertController.Style.alert
+            title: alertTitle, message: alertMessage, preferredStyle: UIAlertController.Style.alert
         )
-        // OKボタン
+
         let defaultAction: UIAlertAction = UIAlertAction(
-            title: "OK",
-            style: UIAlertAction.Style.default,
-            handler: nil
+            title: "OK", style: UIAlertAction.Style.default
         )
-        // UIAlertController に Action を追加
+
         alert.addAction(defaultAction)
-        // Alertを表示
-        present(alert, animated: true, completion: nil)
+        present(alert, animated: true)
     }
     
+    func getLocationInfo(completion: @escaping (Int) -> Void) {
+        guard let url = URLComponents(string: urlString) else { return }
+        
+        let task = URLSession.shared.dataTask(with: url.url!) {(data, response, error) in
+            if (error != nil) {
+                print(error!.localizedDescription)
+            }
+            guard let data = data else { return }
+
+            let response = try! JSONDecoder().decode(Response.self, from: data)
+            print(response)
+            
+            for user in response.data {
+                let name = user.name
+                let isHome = user.isHome ? "家いる" : "家いない"
+                let txt = name + " : " + isHome + "\n"
+
+                if ((self.usersIeiru) == nil) {
+                    self.usersIeiru = txt
+                } else {
+                    self.usersIeiru = self.usersIeiru! + txt
+                }
+            }
+            completion(response.data.count)
+        }
+        task.resume()
+    }
+    
+    func postLocationInfo() {
+        guard let url = URL(string: urlString) else { return }
+        let registeredName = UserDefaults.standard.string(forKey: "name")
+        
+        let parameters: [String: Any] = [
+            "user": [
+                "name": registeredName ?? nameTextField.text,
+                "latitude" : self.latitude,
+                "longitude": self.longitude
+            ]
+        ]
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("Application/json", forHTTPHeaderField: "Content-Type")
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else {
+            return
+        }
+        request.httpBody = httpBody
+        request.timeoutInterval = 20
+        let session = URLSession.shared
+        session.dataTask(with: request) { (data, response, error) in
+            if let response = response {
+                print(response)
+            }
+            if let data = data {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    UserDefaults.standard.set(self.nameTextField.text, forKey: "name")
+                    print(json)
+                } catch {
+                    print(error)
+                }
+            }
+        }.resume()
+    }
 }
 
-extension ViewController: CLLocationManagerDelegate {
-    
-    /// 位置情報が更新された際、位置情報を格納する
-    /// - Parameters:
-    ///   - manager: ロケーションマネージャ
-    ///   - locations: 位置情報
+extension LocationViewController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let location = locations.first
-        let latitude = location?.coordinate.latitude
-        let longitude = location?.coordinate.longitude
-        // 位置情報を格納する
-        self.latitudeNow = String(latitude!)
-        self.longitudeNow = String(longitude!)
+        self.latitude = location?.coordinate.latitude
+        self.longitude = location?.coordinate.longitude
+        self.usersLabel.text = usersIeiru
+        
+        postLocationInfo()
+        print(self.latitude)
+        print(self.longitude)
+        print(usersIeiru)
     }
     
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("うんこおおおおおおおおおおお")
+    }
 }
-/// "クリア"ボタンを押下した際、ラベルを初期化する
-/// - Parameter sender: "クリア"ボタン
